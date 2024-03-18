bd655f497f8ebbb83ada902bf4962be11abf4aba
Bousmaha Wassel <wassel2005@gmail.com>
2024-03-18 19:03:26 +0100

    added support for Netlify contact form

--- script.js
+++ script.js
@@ -9,37 +9,37 @@ function navigate(selectedValue) {
     }
 }
 
-    $(document).ready(function() {
-var scrollContent = $('#scrollContent');
-var current_panel = "hey";
-$("#slide_"+current_panel).addClass("active");
-var panels = Object.fromEntries( $(".panel").get().map( x => [x.id, x]) );
-$(window).scroll(function() {
-    var scrollPosition = $(this).scrollTop();
-    var new_panel = current_panel;
-    var best_cand = Number.MAX_VALUE;
-    for (const [name, panel] of Object.entries(panels)) {
-        if (panel.offsetTop+panel.offsetHeight - window.innerHeight/4 - scrollPosition < 0) {
-            continue;
+$(document).ready(function () {
+    var scrollContent = $('#scrollContent');
+    var current_panel = "hey";
+    $("#slide_" + current_panel).addClass("active");
+    var panels = Object.fromEntries($(".panel").get().map(x => [x.id, x]));
+    $(window).scroll(function () {
+        var scrollPosition = $(this).scrollTop();
+        var new_panel = current_panel;
+        var best_cand = Number.MAX_VALUE;
+        for (const [name, panel] of Object.entries(panels)) {
+            if (panel.offsetTop + panel.offsetHeight - window.innerHeight / 4 - scrollPosition < 0) {
+                continue;
+            }
+            if (panel.offsetTop + panel.offsetHeight - window.innerHeight / 4 - scrollPosition < best_cand) {
+                best_cand = panel.offsetTop - scrollPosition;
+                new_panel = name;
+            }
         }
-        if (panel.offsetTop + panel.offsetHeight - window.innerHeight/4 - scrollPosition < best_cand) {
-            best_cand = panel.offsetTop - scrollPosition;
-            new_panel = name;
+        if (new_panel != current_panel) {
+            $("#slide_" + current_panel).removeClass("active");
+            $("#slide_" + new_panel).addClass("active");
+            current_panel = new_panel;
         }
-    }
-    if (new_panel != current_panel) {
-        $("#slide_"+current_panel).removeClass("active");
-        $("#slide_"+new_panel).addClass("active");
-        current_panel = new_panel;
-    }
-});
+    });
 
-$('#mobileDropdown').focus();
+    $('#mobileDropdown').focus();
 
-//Handle navigation on mobile : 
-$('#mobileDropdown').on('change', function() {
-    var selectedValue = $(this).val();
-    navigate(selectedValue);
-});
+//Handle navigation on mobile :
+    $('#mobileDropdown').on('change', function () {
+        var selectedValue = $(this).val();
+        navigate(selectedValue);
+    });
 
 });
